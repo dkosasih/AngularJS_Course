66ae7b44d2f1369f9b0fd186b5c70d64d917bca6
Deddy Kosasih <deddy.kosasih@ICAREHEALTH.COM>
2016-10-14 17:20:23 +1100

    assignment done

--- module2/boughtList.controller.js
+++ module2/boughtList.controller.js
@@ -2,22 +2,13 @@
 "use strict";
 
 var ctrl = angular.module('app')
-.controller('boughtList', function(dataService){
+.controller('boughtListController', function(dataService){
 	var vm = this;
-	vm.selectedShopItem = [];
-		vm.selectedBoughtItem = [];
+	vm.selectedBoughtItem = [];
+	vm.boughtList = [];
 	
 	var init = function(){
-		vm.shopItems = dataService.getShopItems();
 		vm.boughtList =  dataService.getBoughtList();
-	}
-	
-	vm.shopItemChecked = function (isChecked, item){
-		if(isChecked){
-			vm.selectedShopItem.push(item);
-		}else{
-			vm.selectedShopItem.splice(vm.selectedShopItem.indexOf(item), 1);
-		}
 	}
 	
 	vm.boughtItemChecked = function (isChecked, item){
@@ -28,26 +19,15 @@ var ctrl = angular.module('app')
 		}
 	}
 	
-	vm.moveRight = function(){
-		for (var i = 0; i < vm.selectedShopItem.length; i++){
-				dataService.addBoughtItem(vm.selectedShopItem[i]);
-				dataService.removeShopItem(vm.selectedShopItem[i]);
-		}
-		vm.selectedShopItem = [];
-		vm.btnGrpShop = {};
-		vm.btnGrpBought = {};
-	}
-	
 	vm.moveLeft = function(){
 		for (var i = 0; i < vm.selectedBoughtItem.length; i++){
 				dataService.addShopItem(vm.selectedBoughtItem[i]);
 				dataService.removeBoughtItem(vm.selectedBoughtItem[i]);
 		}
 		vm.selectedBoughtItem = [];		
-		vm.btnGrpShop = {};
 		vm.btnGrpBought = {};
 	}
-
+	
 	init();
 });
 
--- module2/shoppingList.controller.js
+++ module2/shoppingList.controller.js
@@ -2,14 +2,13 @@
 "use strict";
 
 var ctrl = angular.module('app')
-.controller('shoppingList', function(dataService){
+.controller('shoppingListController', function(dataService){
 	var vm = this;
 	vm.selectedShopItem = [];
-		vm.selectedBoughtItem = [];
+	vm.shopItems = [];
 	
 	var init = function(){
 		vm.shopItems = dataService.getShopItems();
-		vm.boughtList =  dataService.getBoughtList();
 	}
 	
 	vm.shopItemChecked = function (isChecked, item){
@@ -20,14 +19,6 @@ var ctrl = angular.module('app')
 		}
 	}
 	
-	vm.boughtItemChecked = function (isChecked, item){
-		if(isChecked){
-			vm.selectedBoughtItem.push(item);
-		}else{
-			vm.selectedBoughtItem.splice(vm.selectedShopItem.indexOf(item), 1);
-		}
-	}
-	
 	vm.moveRight = function(){
 		for (var i = 0; i < vm.selectedShopItem.length; i++){
 				dataService.addBoughtItem(vm.selectedShopItem[i]);
@@ -35,19 +26,8 @@ var ctrl = angular.module('app')
 		}
 		vm.selectedShopItem = [];
 		vm.btnGrpShop = {};
-		vm.btnGrpBought = {};
 	}
 	
-	vm.moveLeft = function(){
-		for (var i = 0; i < vm.selectedBoughtItem.length; i++){
-				dataService.addShopItem(vm.selectedBoughtItem[i]);
-				dataService.removeBoughtItem(vm.selectedBoughtItem[i]);
-		}
-		vm.selectedBoughtItem = [];		
-		vm.btnGrpShop = {};
-		vm.btnGrpBought = {};
-	}
-
 	init();
 });
 
