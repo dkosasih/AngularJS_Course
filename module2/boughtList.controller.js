f20a1f333b7985b92822754364448dca52d2c92e
Deddy Kosasih <deddy.kosasih@ICAREHEALTH.COM>
2016-10-14 16:32:21 +1100

    initial submission of module 2

---
+++ module2/boughtList.controller.js
@@ -0,0 +1,58 @@
+(function(){
+"use strict";
+
+var ctrl = angular.module('app')
+.controller('boughtList', function(dataService){
+	var vm = this;
+	vm.selectedShopItem = [];
+		vm.selectedBoughtItem = [];
+	
+	var init = function(){
+		vm.shopItems = dataService.getShopItems();
+		vm.boughtList =  dataService.getBoughtList();
+	}
+	
+	vm.shopItemChecked = function (isChecked, item){
+		if(isChecked){
+			vm.selectedShopItem.push(item);
+		}else{
+			vm.selectedShopItem.splice(vm.selectedShopItem.indexOf(item), 1);
+		}
+	}
+	
+	vm.boughtItemChecked = function (isChecked, item){
+		if(isChecked){
+			vm.selectedBoughtItem.push(item);
+		}else{
+			vm.selectedBoughtItem.splice(vm.selectedShopItem.indexOf(item), 1);
+		}
+	}
+	
+	vm.moveRight = function(){
+		for (var i = 0; i < vm.selectedShopItem.length; i++){
+				dataService.addBoughtItem(vm.selectedShopItem[i]);
+				dataService.removeShopItem(vm.selectedShopItem[i]);
+		}
+		vm.selectedShopItem = [];
+		vm.btnGrpShop = {};
+		vm.btnGrpBought = {};
+	}
+	
+	vm.moveLeft = function(){
+		for (var i = 0; i < vm.selectedBoughtItem.length; i++){
+				dataService.addShopItem(vm.selectedBoughtItem[i]);
+				dataService.removeBoughtItem(vm.selectedBoughtItem[i]);
+		}
+		vm.selectedBoughtItem = [];		
+		vm.btnGrpShop = {};
+		vm.btnGrpBought = {};
+	}
+
+	init();
+});
+
+ctrl.$inject = ['dataService'];
+
+return ctrl;
+
+})();
---
+++ module2/data.service.js
@@ -0,0 +1,59 @@
+(function(){
+"use strict";
+
+var serv = angular.module('app')
+.service('dataService', function(){
+	var service = this;
+	var shopStocks = [
+						{
+							id: 1,
+							name: 'Bismol Pepto',
+							category: 'Medicine',
+							quantity: 10
+						},
+						{
+							id: 2,
+							name: 'Cookie',
+							category: 'food',
+							quantity: 3
+						}
+						,
+						{
+							id: 3,
+							name: 'Chips',
+							category: 'food',
+							quantity: 4
+						}
+					];
+	var boughtList = [];
+	
+	 service.getShopItems = function() {
+		return shopStocks;
+	};
+	
+	service.getBoughtList = function() {
+		return boughtList;
+	};
+	
+	service.addBoughtItem = function(itemToBuy){
+		boughtList.push(itemToBuy);
+	};
+	
+	service.removeBoughtItem = function(itemToRemove){
+		boughtList.splice(boughtList.indexOf(itemToRemove), 1);
+	};
+	
+	service.addShopItem = function(itemToBuy){
+		shopStocks.push(itemToBuy);
+	};
+	
+	service.removeShopItem = function(itemToRemove){
+		shopStocks.splice(shopStocks.indexOf(itemToRemove), 1);
+	};
+	
+	
+});
+
+return serv;
+
+})();
